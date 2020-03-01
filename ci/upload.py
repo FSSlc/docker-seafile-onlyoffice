dbd29f4a723c93ff04eeda3c80b27541ead98002
FSSlc <fsslc235@gmail.com>
2020-03-01 21:57:08 +0800

    Only support python 2
    Signed-off-by: FSSlc <fsslc235@gmail.com>

--- ci/upload.py
+++ ci/upload.py
@@ -2,12 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import urllib
-
-try:
-    import urllib2
-except ImportError:
-    import urllib.request as urllib2
-
+import urllib2
 import json
 import requests
 import sys
@@ -20,7 +15,7 @@ username = 'me@example.com'
 password = 'asecret'
 get_token_url = '{}/api2/auth-token/'.format(hostname)
 data = urllib.urlencode({'username': username, 'password': password})
-request = urllib2.Request(get_token_url,data)
+request = urllib2.Request(get_token_url, data)
 response = urllib2.urlopen(request)
 _js_py = json.load(response)
 token = _js_py['token']
@@ -30,7 +25,7 @@ response.close()
 Generate default repo_id
 """
 get_library_url = '{}/api2/default-repo/'.format(hostname)
-headers = {'Authorization': 'Token ' + token, 'Connection':'close'}
+headers = {'Authorization': 'Token ' + token, 'Connection': 'close'}
 r = requests.post(get_library_url, headers=headers)
 assert r.status_code == 200
 
@@ -39,7 +34,7 @@ Get default repo_id
 """
 get_library_url = '{}/api2/default-repo/'.format(hostname)
 get_repo_id = urllib2.Request(get_library_url)
-get_repo_id.add_header('Authorization','Token ' + token)
+get_repo_id.add_header('Authorization', 'Token ' + token)
 response_repo_id = urllib2.urlopen(get_repo_id)
 _js_py = json.load(response_repo_id)
 repo_id = _js_py['repo_id']
@@ -50,9 +45,10 @@ Get upload link
 """
 upload_link_url = '{}/api2/repos/{}/upload-link/'.format(hostname, repo_id)
 get_upload_link = urllib2.Request(upload_link_url)
-get_upload_link.add_header('Authorization','Token ' + token)
+get_upload_link.add_header('Authorization', 'Token ' + token)
 response_upload_link = urllib2.urlopen(get_upload_link)
-upload_link = json.load(response_upload_link).replace('seafile.example.com', '127.0.0.1').replace('https', 'http')
+upload_link = json.load(response_upload_link).replace(
+    'seafile.example.com', '127.0.0.1').replace('https', 'http')
 response_upload_link.close()
 
 """
@@ -63,7 +59,6 @@ filename = sys.argv[1]
 url = upload_link
 files = {'file': open(filename, 'rb')}
 r = requests.post(
-		url, data={'filename': filename, 'parent_dir': '/'},
-		files=files, headers={'Authorization': 'Token ' + token})
+    url, data={'filename': filename, 'parent_dir': '/'},
+    files=files, headers={'Authorization': 'Token ' + token})
 files['file'].close()
-
